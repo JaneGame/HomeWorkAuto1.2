d3b69d5607bd9a9dca60a595a590969b39a3e348
JaneGame <fibi92@mail.ru>
2021-10-18 20:58:13 +0300

    TestMax

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -7,12 +7,21 @@ public class CashbackHackerTest {
     CashbackHackService hacker = new CashbackHackService();
 
     @Test
-public void cashbackTest(){
+public void cashbackTest() {
 
         int actual = hacker.remain(100);
         int expected = 900;
 
-Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+        @Test
+        public void cashbackTestMax(){
+            int actual = hacker.remain(1000);
+
+            int expected = 0;
+
+            Assert.assertEquals(actual, expected);
 
     }
 }
